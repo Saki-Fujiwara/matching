05cf3cd173a85600962730a5b4686f2735f3c952
Saki-Fujiwara <saki.0902s.f@gmail.com>
2024-02-27 08:38:36 +0900

    npmの記載

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
+        @csrf
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 

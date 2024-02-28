fc547b657d219b96bdf68d7dc17ca0f046cf42ad
Saki-Fujiwara <saki.0902s.f@gmail.com>
2024-02-28 18:41:40 +0900

    初期画面のデザイン変更

--- resources/views/auth/default.blade.php
+++ resources/views/auth/default.blade.php
@@ -5,32 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログインと新規登録</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
     <style>
-        /* 任意のカスタムCSSスタイルを追加 */
+     body {
+        color: #651900;
+        font-size: 30px;
+        font-family: Arial, sans-serif;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #f0f0f0;
+        text-align: center;
+    }
+    .button {
+        background-color: #f5deb3;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        transition: transform 0.3s ease;
+        border-radius: 5px;
+    }
+    .button:hover {
+        transform: scale(1.2);
+    }
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-6">
-                <h1>MACHI MACHIへようこそ</h1>
-                
-                
-                <h2 class="mb-4">ログイン</h2>
-                    <a href="{{ route('login') }}" class="btn btn-primary btn-block">ログイン</a>
-                </form>
-            </div>
-        </div>
-        <div class="row justify-content-center mt-3">
-            <div class="col-md-6">
-                <h2 class="mb-4">新規登録</h2>
+    <h1>MACHI MACHIへようこそ</h1>
+        <h2 class="">ログイン</h2>
+            <a href="{{ route('login') }}"type="submit" class="button">ログイン</a>
+                <h2>新規登録</h2>
                 <p>アカウントをお持ちでない場合は、新規登録してください。</p>
-                <a href="{{ route('register') }}" class="btn btn-success btn-block">新規登録</a>
+                <a href="{{ route('register') }}"type="submit" class="button">新規登録</a>
             </div>
         </div>
     </div>
 
     <!-- Bootstrap JavaScript (Optional) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/c
+
+</body>
